5a26ba3c8513f814932f59eb688636734008a39c
albenchris <albenchris00@gmail.com>
2021-03-22 16:21:15 -0500

    finished draggable function

--- script.js
+++ script.js
@@ -14,7 +14,36 @@ draggables.forEach(draggable => {
 containers.forEach(container => {
     container.addEventListener("dragover", event => {
         event.preventDefault();
+
+        const afterElement = getDragAfterElement(container, event.clientY);
+        // console.log(afterElement);
+
         const draggable = document.querySelector(".dragging");
-        container.appendChild(draggable);
+
+        if (afterElement == null) {
+            container.appendChild(draggable);
+        } else {
+            container.insertBefore(draggable, afterElement);
+        }
     });
 });
+
+function getDragAfterElement(container, y) {
+    const draggableElements = [...container.querySelectorAll(".draggable:not(.dragging)")];
+
+    return draggableElements.reduce((closest, child) => {
+        const box = child.getBoundingClientRect();
+        const offset = y - box.top - box.height / 2;
+        // console.log(box);
+        // console.log(offset);
+
+        if (offset < 0 && offset > closest.offset) {
+            return { 
+                offset: offset,
+                element: child
+            }
+        } else {
+            return closest;
+        }
+    }, { offset: Number.NEGATIVE_INFINITY }).element;
+}
